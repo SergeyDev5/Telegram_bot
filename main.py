98ac46910672cf53bf5b2fa22c727d32e260fcb0
Sergey <serega98.0045@gmail.com>
2024-10-29 20:20:03 +1000

    Add: tab

--- main.py
+++ main.py
@@ -27,23 +27,29 @@ def get_audio_messages(message):
         path = os.path.splitext(file_info.file_path)[0]
         file_name = os.path.basename(path)
         doc = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
+        
         with open(file_name + '.oga', 'wb') as f:
             f.write(doc.content)  # save audio message
         process = subprocess.run(['ffmpeg', '-i', file_name + '.oga', file_name + '.wav'])
         result = audio_to_text(file_name + '.wav')
         bot.send_message(message.chat.id, format(result))
+        
     except sr.UnknownValueError as e:
         bot.send_message(message.chat.id, "Не разобрал сообщение...")
+        
         with open(logfile, 'a', encoding='utf-8') as f:  # save audio
             f.write(str(datetime.datetime.today().strftime("%H:%M:%S")) + ':' + str(message.from_user.id) + ':' + str(
                 message.from_user.first_name) + '_' + str(message.from_user.last_name) + ':' + str(
                 message.from_user.username) + ':' + str(message.from_user.language_code) + ':Message is empty.\n')
+            
     except Exception as e:  # wrong, if speech was don't understand
         bot.send_message(message.chat.id, "Упс, ошибка")
+        
         with open(logfile, 'a', encoding='utf-8') as f:  # if any other problem
             f.write(str(datetime.datetime.today().strftime("%H:%M:%S")) + ':' + str(message.from_user.id) + ':' + str(
                 message.from_user.first_name) + '_' + str(message.from_user.last_name) + ':' + str(
                 message.from_user.username) + ':' + str(message.from_user.language_code) + ':' + str(e) + '\n')
+            
     finally:  # deleting temporary files
         os.remove(file_name + '.wav')
         os.remove(file_name + '.oga')
